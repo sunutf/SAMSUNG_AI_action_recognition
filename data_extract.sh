8ffd0ef6ab1f9e4faf19f6995e3ba77fdd8b6441
trash-jhseon <sunutf@gmail.com>
2021-07-15 10:18:21 +0000

    add data_extract shell

---
+++ data_extract.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+mkdir ~/SAMSUNG_AI_action_recognition/2_C3D_nonlocal/dataset
+cd /home
+cp action_youtube_naudio.tar.gz ~/SAMSUNG_AI_action_recognition/2_C3D_nonlocal/dataset
+cd ~/SAMSUNG_AI_action_recognition/2_C3D_nonlocal/dataset
+tar -xvf action_youtube_naudio.tar.gz
